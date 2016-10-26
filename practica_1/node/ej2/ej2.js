96cea3ff9af52ad6243210551941aaafee46053a
orioliz <oriolizquierdovi@gmail.com>
2016-10-26 19:11:06 +0200

    comment

---
+++ practica_1/node/ej2/ej2.js
@@ -0,0 +1,9 @@
+var mates =require('./matematicas'); // nombre a exportar
+var num1=24;
+var num2=40;
+
+var calc=new Mates();
+
+
+calc.suma(num1,num2);
+
---
+++ practica_1/node/matematicas/index.js
@@ -0,0 +1,14 @@
+var Mates=function() {
+	this.operador1=0;
+	this.operador2=0;
+
+	this.suma=function(op1,op2){
+		var res=op1+op2;
+		console.log('El resultado de '+op1+' y '+op2+' es:'+res);
+		return
+	};
+
+};
+
+
+module.exports=Mates;
